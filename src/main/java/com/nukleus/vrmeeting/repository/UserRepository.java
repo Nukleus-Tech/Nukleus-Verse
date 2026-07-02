0dbb88a5692ac3bb3b58df7ce3d99863322c4ad7
Nukleus-Tech <tech@nukleus.work>
2026-07-02 12:46:19 +0530

    Update meeting and user models for current meeting tracking

--- src/main/java/com/nukleus/vrmeeting/repository/UserRepository.java
+++ src/main/java/com/nukleus/vrmeeting/repository/UserRepository.java
@@ -2,9 +2,13 @@ package com.nukleus.vrmeeting.repository;
 
 import com.nukleus.vrmeeting.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    User findByEmail(String email);
+
     User findByEmailIgnoreCase(String email);
 
+    List<User> findByCurrentMeetingId(String currentMeetingId);
 }
