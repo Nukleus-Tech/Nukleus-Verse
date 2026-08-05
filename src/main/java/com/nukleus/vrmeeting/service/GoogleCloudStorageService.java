388308a4192e81bbb57a60836fa7ee2426e8da6a
Nukleus-Tech <tech@nukleus.work>
2026-08-05 17:18:38 +0530

    Refactor GoogleCloudStorageService for improved readability and code organization

--- src/main/java/com/nukleus/vrmeeting/service/GoogleCloudStorageService.java
+++ src/main/java/com/nukleus/vrmeeting/service/GoogleCloudStorageService.java
@@ -1,57 +1,48 @@
 package com.nukleus.vrmeeting.service;
 
-
 import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Storage;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.net.URL;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
-
 
 @Service
 public class GoogleCloudStorageService {
 
+        @Value("${gcp.bucket-name}")
+        private String bucketName;
 
-    @Value("${gcp.bucket-name}")
-    private String bucketName;
+        private final Storage storage;
 
+        public GoogleCloudStorageService(Storage storage) {
 
-    private final Storage storage;
+                this.storage = storage;
 
+        }
 
-    public GoogleCloudStorageService(Storage storage) {
+        public String generateDownloadUrl(String fileName) {
 
-        this.storage = storage;
+                BlobInfo blobInfo = BlobInfo.newBuilder(
+                                bucketName,
+                                fileName).build();
 
-    }
+                URL url = storage.signUrl(
+                                blobInfo,
+                                1,
+                                TimeUnit.HOURS,
+                                Storage.SignUrlOption.withV4Signature(),
+                                Storage.SignUrlOption.withExtHeaders(
+                                                Map.of(
+                                                                "Content-Disposition",
+                                                                "attachment; filename=\"" +
+                                                                                fileName.substring(fileName
+                                                                                                .lastIndexOf("/") + 1)
+                                                                                + "\"")));
 
-
-
-    public String generateDownloadUrl(String fileName) {
-
-
-        BlobInfo blobInfo =
-                BlobInfo.newBuilder(
-                        bucketName,
-                        fileName
-                )
-                .build();
-
-
-
-        URL url =
-                storage.signUrl(
-                        blobInfo,
-                        1,
-                        TimeUnit.HOURS,
-                        Storage.SignUrlOption.withV4Signature()
-                );
-
-
-        return url.toString();
-
-    }
+                return url.toString();
+        }
 
 }
